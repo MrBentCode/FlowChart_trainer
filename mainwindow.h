ab98072dc543da6fbb3849fded34d3a3e1631388
AlinaSt <alina-st@hotmail.com>
2015-10-25 11:39:21 +0300

    Adds QWordCompleter

--- mainwindow.h
+++ mainwindow.h
@@ -11,6 +11,7 @@
 //#include <QWebView>
 #include "testwindow.h"
 #include "exercisewindow.h"
+#include "qwordcompleter.h"
 
 namespace Ui {
 class MainWindow;
@@ -65,6 +66,7 @@ private:
     QTextBrowser *theoryBrowser;
     QString theory;
     QString html;
+    QCompleter* completer;
 
     void createActions();
     void createMenus();
